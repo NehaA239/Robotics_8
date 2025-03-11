b3c77779b586cec7fb81c73a610a966310e974ed
NehaA239 <NehaA239@stemcollegiate.ca>
2025-03-11 13:58:59 -0600

    Update and rename March_10_day5.py to March_11_day5.py

---
+++ March_11_day5.py
@@ -0,0 +1,51 @@
+# today I learned how to use the if or else function, and I mak=de a small test which has questions.
+myName = input("What's your name?: ")
+if myName == "David":
+ print("Welcome Dude!")
+ print("You're just the baldest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
+
+catsOrDogs=input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+drink = input("Do you prefer coffee or tea?")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
+
+print("Which Character are you?")
+print()
+Spiderman=input("Do you like to shoot webs? ")
+if Spiderman == "yes":
+  print("You are spiderman!")
+else:
+  print("You are not spiderman")
+print()
+Ironman=input("Are you strong? ")
+if Ironman == "yes":
+  print("OMG! You are Hulk")
+else:
+  print("Dang, you aren't Hulk")
+print()
+PercyJackson=input("Are you born on August 18th? ")
+if PercyJackson == "yes":
+  print("Wow, your Percy Jackson!")
+else:
+  print("That's sad, you aren't Percy Jackson")
+print()
+Rarity=input("Are you a girl?")
+if Rarity == "yes":
+  print("This is AMAZING! you are Rarity!")
+else:
+  print("You are not Rarity")
