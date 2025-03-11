ccb574c6e9f5c839a2de90b68046bcf35539dc20
NehaA239 <NehaA239@stemcollegiate.ca>
2025-03-10 20:20:25 -0600

    Create March_10_day4.py

---
+++ March_10_day4.py
@@ -0,0 +1,8 @@
+# I learned how to make long stories, adding variables and everything.
+Name=input("What's you name: ")
+Pet=input("What's your pet's name: ")
+Creature=input("What's your favorite mythical creature: ")
+Hobby=input("What's your favorite hobby: ")
+Destination=input("What's your dream destination: ")
+print()
+print("In the amazing city of",Destination,",",Name, ",an adventrous traveler with love for", Hobby,", embarked on an amazing journey with her pet", Creature, "named", Pet, "to find the hidden treasure of", Destination, "and save the day.")
