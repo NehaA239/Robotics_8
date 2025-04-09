9e5cce4ffb6cf700ac7eedd6ad1941105ea66963
NehaA239 <NehaA239@stemcollegiate.ca>
2025-04-08 21:32:17 -0600

    Update Obstacle_Course_April8.py

--- Obstacle_Course_April8.py
+++ Obstacle_Course_April8.py
@@ -6,12 +6,17 @@ drivetrain.turn_for(RIGHT, 15, DEGREES)
 drivetrain.drive_for(FORWARD, 400, MM)
 drivetrain.turn_for(LEFT, 25, DEGREES)
 drivetrain.drive_for(FORWARD, 300, MM)
+# The codes above are for navigating through the red balls at the start
 drivetrain.turn_for(LEFT, 70, DEGREES)
 drivetrain.drive_for(FORWARD, 100, MM)
 drivetrain.turn_for(LEFT, 110, DEGREES)
+#The following ones above are for passing through the wall and going through the wall.
 drivetrain.drive_for(FORWARD, 1100, MM)
+#The line above is for move a blue ball into a goal, I don't know how to highlight it though.
 drivetrain.drive_for(REVERSE, 1100, MM)
 drivetrain.turn_for(RIGHT, 90, DEGREES)
+# Reversing through the goal and turning towards the seesaw.
 drivetrain.drive_for(FORWARD, 500, MM)
 drivetrain.turn_for(LEFT, 90, DEGREES)
 drivetrain.drive_for(FORWARD, 1100, MM)
+# going up the seesaw and to the finish line.
